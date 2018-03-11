16a09d11b65f060200df14cb3589ed6d74d9d1ee
Mapi Pina <mpina09@gmail.com>
2018-03-11 19:57:44 -0400

    sent high level player data to Firebase

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,6 +14,12 @@
   // need variables for player 1 and player 2
   var playerONE = "";
  // var playerTWO = "";
+
+// needs to be variables for wins and losses that is recorded in the database
+  var p1Wins = 0;
+  p1Losses = 0;
+  // p2Wins = 0;
+  // p2Losses = 0;
   // variables need to take the value from the form box
   $("form").eq(0).submit(function(event){
     event.preventDefault();
@@ -21,17 +27,19 @@
     playerONE = userInput;
     console.log(playerONE);
     console.log(userInput);
+
+    database.ref().push({
+      name: playerONE,
+      wins: p1Wins,
+      losses: p1Losses
+    })
   });
 
   // need to update the player box to reflect name and choices
   // we need to update Firebase to upload the names of these players
   // max of 2 players allowed
   // there needs to be a toggle jquery untion for Hi NAME! You are Player i
-  // needs to be variables for wins and losses that is recorded in the database
-  var p1Wins = 0;
-  p1Losses = 0;
-  p2Wins = 0;
-  p2Losses = 0;
+
   // needs to be toggle function for wins and losses at the bottom of each player box
   // need record object for player
-  var players = [{losses: 0, name:"", wins: 0,}]
+  // var players = [{losses: 0, name:"", wins: 0,}]
