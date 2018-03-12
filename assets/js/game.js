36a6bcc9a56f5b2a9bbcf2d8bf331c83c10519c6
Mapi Pina <mpina09@gmail.com>
2018-03-12 16:33:59 -0400

    manipulating the DOM with player1 info, havng a hard time adding p2 info

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,13 +13,14 @@
 
   // need variables for player 1 and player 2
   var playerONE = "";
- // var playerTWO = "";
+  var playerTWO = "";
 
 // needs to be variables for wins and losses that is recorded in the database
   var p1Wins = 0;
   p1Losses = 0;
-  // p2Wins = 0;
-  // p2Losses = 0;
+  p2Wins = 0;
+  p2Losses = 0;
+// function to toggle the form once player one is selected
   var toggleForm = function(){
     $("form").hide();
     var greeting = "Hi " + playerONE + "! You are Player 1"
@@ -37,7 +38,7 @@
   // need to inform user that they are player i
     toggleForm();
   // we need to update Firebase to upload the names of these players
-    database.ref('players').push({
+    database.ref('players').child('player1').update({
       name: playerONE, 
       wins: p1Wins, 
       losses: p1Losses})
@@ -45,16 +46,51 @@
 
 
   // need to update the player box to reflect name and choices
-  database.ref('players/').on("value", function(snapshot){
+  database.ref('players/').child('player1').on("value", function(snapshot){
     //console logging everything first 
     console.log(snapshot.val());
     console.log(snapshot.val().name);
     console.log(snapshot.val().wins);
     console.log(snapshot.val().losses);
+
+    var player1Name = (snapshot.val().name);
+    wins = (snapshot.val().wins);
+    losses = (snapshot.val().losses);
+    $("#p1").text(player1Name);
+    $("#p1-box").append("<div>" + "Wins:" + wins + " Losses:" + losses + "</div>");
   });
+
+  // Repeat everything for second player. Need IF statement
+  var newUser = function() {
+    $("form").submit(function(event){
+      event.preventDefault();
+      var userInput = $("#name-input").val();
+      playerTWO = userInput;
+    // clear out the name form
+      $("#name-input").val("");
+    // need to inform user that they are player i
+      $("form").hide();
+      var greeting = "Hi " + playerTWO + "! You are Player 2"
+      $("#form").text(greeting);
+    // we need to update Firebase to upload the names of these players
+      database.ref('players/').child('player2').update({
+        name: playerTWO, 
+        wins: p2Wins, 
+        losses: p2Losses})
+    });
+  };
+
+
+  function updatePlayerTWO () {
+  //  var record = database.ref('players/').child('player1');
+    if (playerONE !== null) {
+      newUser();
+    }
+    };
 
 
   // max of 2 players allowed
-  // there needs to be a toggle jquery untion for Hi NAME! You are Player i
+  // there needs to be a toggle jquery function for Hi NAME! You are Player i
 
   // needs to be toggle function for wins and losses at the bottom of each player box
+  // function for gameplay once both players are live
